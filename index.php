b6b6e78ac9d70c8ced7b93d8f639a6a33a0de0ef
madalina11 <madalina11_01@yahoo.com>
2017-06-12 09:26:16 +0300

    acasa

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE HTML>
 <html lang="ro">
     <?php include('./lib/components/meta.php') ?>
@@ -14,19 +10,18 @@
 				<div class="page">
 					<?php 
 						if (getUser()) {
-						 {
 							echo '
-								<h4>Bine ați venit!</h4>
+								<h4>Bine ați revenit, ' . $_SESSION["user"]["firstname"] . ' ' . $_SESSION["user"]["lastname"] . '!</h4>
 
 								<div class="presentation">
-									<img src="./public/res/acasa_guest.jpg"/>
+									<img src="./public/res/acasa_logged.jpg"/>
 								</div>
 
 								<p class="text-center">
-									Acesta este un sit realizat cu scopul de ajuta persoanele să scape de diferite fobii.<br/>
-									Pentru utilizarea aplicației, va trebui să vă înregistrați un cont, iar pe urmă să vă autentificați.<br/><br/>
-									În cazul în care doriți ajutor privat, nu ezitați să ne contactați la numarul de telefon: <br/>
-									+(40)425.521.522
+									<u>Fobiile ce le aveți:</u><br/>
+									' . $_SESSION["user"]["phobia"] . '<br/><br/><br/>
+									Va trebui sa persistați ca să puteți scăpa de fobiile dumneavoastră.<br/>
+									Nu vă dați bătut și continuați cu excercițiile oferite de noi!
 								</p>
 							';
 						}
